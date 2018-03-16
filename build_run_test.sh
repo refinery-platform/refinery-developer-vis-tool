ff6a7602348f8cc18f098a39009fa714a98ca063
Scott Ouellette <scottx611x@gmail.com>
2018-03-16 15:42:39 -0400

    Change test data url to something that isn't rate-limited

--- build_run_test.sh
+++ build_run_test.sh
@@ -8,7 +8,7 @@ docker pull $REPO
 docker build --tag $NAME \
              --cache-from $REPO \
              context
-docker run --env INPUT_JSON_URL=https://api.github.com/users/scottx611x/repos \
+docker run --env INPUT_JSON_URL=http://data.cloud.refinery-platform.org.s3.amazonaws.com/data/scott/sample.json \
            --detach \
            --name $NAME \
            --publish 80 \
