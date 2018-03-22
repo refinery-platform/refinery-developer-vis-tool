779e5c7031646318e1a0fa5ddc8c76dc4ae7dbba
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-03-22 12:06:16 -0400

    This is a better model to build from

--- context/on_startup.py
+++ context/on_startup.py
@@ -4,16 +4,18 @@ import requests
 
 if __name__ =="__main__":
     # input.json should just be in place already, if it has been given.
-    # Using environ.get to be a little more robust against missing values.
 
-    with open("envvar_value.json", "w") as f:
-        json = os.environ.get("INPUT_JSON")
-        if json:
+    # If you're using this as template to start from, you probably don't care
+    # which mode was used, and every case should write to the same input.json.
+
+    json = os.environ.get("INPUT_JSON")
+    if json:
+        with open("envvar_value.json", "w") as f:
             f.write(json)
 
-    with open("envvar_url.json", "w") as f:
-        url = os.environ.get("INPUT_JSON_URL")
-        if url:
+    url = os.environ.get("INPUT_JSON_URL")
+    if url:
+        with open("envvar_url.json", "w") as f:
             f.write(requests.get(url).text)
 
     print('envvars read')
